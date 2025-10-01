615fb1fe59ded208c2d9f6dde0124def4e2fcae0
PorVad <143946767+MDPLofficial@users.noreply.github.com>
2025-10-01 19:13:28 -0400

    again

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 
-import { getImagesByQuery } from "./pixabay-api";
-import { createGallery, clearGallery, showLoader, hideLoader } from "./render-functions";
+import { getImagesByQuery } from "./js/pixabay-api";
+import { createGallery, clearGallery, showLoader, hideLoader } from "./js/render-functions";
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
